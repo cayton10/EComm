f87780b8ae1ed6d47b13ac4b473641ec5fecd373
Obi-Wan <cayton10@live.marshall.edu>
2020-08-24 20:54:03 -0400

    Dynamic Year

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+//This file will serve as my custom jQuery commands file
+//Include dynamic auto updating date
+$(document).ready(function()
+{
+    //Set dynamic year
+    ('#year').document.write(new Date().getFullYear());
+
+});
